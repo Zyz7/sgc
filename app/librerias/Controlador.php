c2ff907f40005b112f62cf9fe1c06fadc982a6ef
zyz <zyz7@pm.me>
2021-02-02 07:51:51 -0600

    php

--- app/librerias/Controlador.php
+++ app/librerias/Controlador.php
@@ -4,19 +4,19 @@ class Controlador {
 	protected $controlador = "Login";
 	protected $metodo = "vista";
 	protected $parametros = [];
-	
+
 	function __contruct(){
-		
+
 	}
-	
+
 	public function modelo($modelo) {
 		require_once('../app/modelos/'.$modelo);
 		return new $modelo();
 	}
-	
+
 	public function vista($vista, $datos=[]) {
 		if (file_exists('../app/vistas/'.$vista)) {
-			require_once('../app/vistas/'.$vista)
+			require_once('../app/vistas/'.$vista);
 		} else {
 			die('La vsta no existe');
 		}
