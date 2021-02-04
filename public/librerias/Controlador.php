1ddaf58c9ca19cc777a6a64ab060ae54b5e14f8e
Zyz7 <51257260+Zyz7@users.noreply.github.com>
2021-02-03 19:14:44 -0600

    php

---
+++ public/librerias/Controlador.php
@@ -0,0 +1,27 @@
+<?php
+
+class Controlador {
+  protected $controlador = "Login";
+  protected $metodo = "vista";
+  protected $parametros = [];
+
+  function __construct() {
+
+  }
+
+  public function modelo($modelo) {
+	  require_once("../modelos/".$modelo.".php");
+	  return new $modelo();
+  }
+
+  public function vista($vista, $datos=[]) {
+
+    if (file_exists("../vistas/".$vista.".php")) {
+	    require_once("../vistas/".$vista.".php");
+	  } else {
+	    die("La vista no existe");
+	  }
+  }
+}
+
+?>
