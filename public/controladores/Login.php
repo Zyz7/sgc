1d1e07edbcffc4781dc08aa5f66581f4237de3db
Zyz7 <51257260+Zyz7@users.noreply.github.com>
2021-02-03 19:21:25 -0600

    php

---
+++ public/controladores/Login.php
@@ -0,0 +1,15 @@
+<?php
+
+class Login extends Controlador {
+  private $modelo;
+
+  function __construct() {
+
+    $this->modelo = $this->modelo("LoginModelo");
+  }
+
+  function vista() {
+
+	  $this->vista("loginVista");
+  }
+}
