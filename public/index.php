5d0f6f49ec1eddf43b2db48cd5d2a27f5ae45227
Zyz7 <51257260+Zyz7@users.noreply.github.com>
2021-02-04 18:56:18 -0600

    mvc

--- public/index.php
+++ public/index.php
@@ -1,7 +1,8 @@
 <?php
 
-require_once('controladores/Login.php');
-$controlador = new Login;
-$controlador->vista();
+//Si no se puede incluir el archivo se detiene la ejecución
+require_once('../app/inicio.php');
+//Instancia la clase ControlUrl de la carpeta app/librerias
+$control = new ControlUrl();    
 
 ?>
