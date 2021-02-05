9fdc085bcc65c609f6fa023b88f12cdecc18c3bf
zyz <zyz7@pm.me>
2021-02-05 08:20:03 -0600

    php

--- app/vistas/loginVista.php
+++ app/vistas/loginVista.php
@@ -1,17 +1,17 @@
 <!DOCTYPE html>
 <html lang="es">
   <head>
-    <title>{ titulo }</title>
+    <title>{titulo}</title>
     <meta charset="utf-8"/>
     <meta name="author" content=""/>
     <meta http-equiv="x-ua-compatible" content="ie-edge"/>
     <meta name="keywords" content=""/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <meta name="description" content=""/>
-    
+
     <link rel="canonical" href="https://sgcphp.herokuapp.com"/>
   </head>
-  
+
   <body>
     <h1>Hola bruno y sofi</h1>
   </body>
