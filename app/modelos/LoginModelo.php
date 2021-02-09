fbb1eca2b99ce654f7ebfad6dbce6bbc3a3cc6d7
zyz <zyz7@pm.me>
2021-02-09 09:19:11 -0600

    sql

--- app/modelos/LoginModelo.php
+++ app/modelos/LoginModelo.php
@@ -2,10 +2,29 @@
 
 class LoginModelo {
   private $db;
+  private $resultado = false;
 
   //Instancia la clase MysqlConexion de la carpeta app/librerias
   function __construct() {
     $this->db = new MysqlConexion();
+  }
+
+  function registrate($valores) {
+    $hash = password_hash($valores["contraseña"], PASSWORD_BCRYPT);
+
+    $consulta = "insert into usuarios values(";
+    $consulta.= "'".$valores["nombre"]."', ";
+    $consulta.= "'".$valores["apellido"]."', ";
+    $consulta.= "'".$valores["usuario"]."', ";
+    $consulta.= "'".$valores["email"]."', ";
+    $consulta.= "'".$hash."', ";
+    $consulta.= "'', ";
+    $consulta.= "'Activo')";
+
+    if ($this->db->consultaBooleano($consulta)) {
+      $this->$resultado = true;
+    }
+    return $this->$resultado;
   }
 
 }
