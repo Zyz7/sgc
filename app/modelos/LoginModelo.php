32b77231ede8faac0cd78e89d30f232af7cf5e49
zyz <zyz7@pm.me>
2021-02-11 00:03:49 -0600

    php

--- app/modelos/LoginModelo.php
+++ app/modelos/LoginModelo.php
@@ -39,6 +39,36 @@ class LoginModelo {
     return $this->$resultado;
   }
 
+  function enviarEmail($email) {
+    $para = "'".$email."'";
+    $titulo = 'Restablecer contraseña';
+    $mensaje = '
+    <html>
+      <head>
+        <title>Recordatorio de cumpleaños para Agosto</title>
+      </head>
+      <body>
+        <h1>Restablecer contraseña</h1>
+        <p>Da click en:</p>
+        <a href="https://sgcphp.herokuapp.com/login/recuperar/'.$email.
+        "'>restablecer</a>
+      </body>
+    </html>
+    '";
+    $cabeceras  = 'MIME-Version: 1.0' . "\n";
+    $cabeceras .= 'Content-type: text/html; charset=iso-8859-1' . "\n";
+    $cabeceras .= 'From: SGC <soporte@sgc.com>' . "\n";
+
+    $consulta = "select * from usuarios where email='".$valores["email"]."'";
+
+    //if ($this->db->consultaBooleano($consulta)) {
+      if (mail($para, $título, $mensaje, $cabeceras)) {
+        $this->$resultado = true;
+      }
+    //}
+    return $this->$resultado;
+  }
+
 }
 
 ?>
