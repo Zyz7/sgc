0aec049294400c4b3fde9d075604041992abd481
Zyz7 <51257260+Zyz7@users.noreply.github.com>
2021-02-12 19:34:47 -0600

    php

--- app/inicio.php
+++ app/inicio.php
@@ -6,7 +6,6 @@ require_once('librerias/MysqlConexion.php');
 require_once('librerias/Controlador.php');
 require_once('librerias/ControlUrl.php');
 require_once('librerias/Validar.php');
-require_once('librerias/Sesion.php');
 require_once('librerias/Exception.php');
 require_once('librerias/PHPMailer.php');
 require_once('librerias/SMTP.php');
