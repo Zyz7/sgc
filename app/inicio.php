734d40605d0b332d465dfaa82b2cde1526b53f27
Zyz7 <51257260+Zyz7@users.noreply.github.com>
2021-02-18 09:48:19 -0600

    psr

--- app/inicio.php
+++ app/inicio.php
@@ -1,7 +1,19 @@
 <?php
 
+/*
+ * \version 1
+ * \date 2021
+ * \author Mario Alberto Zayas González
+ */
+
+/* 
+ * Define la constante RUTA
+ * En modo local se cambia por el nombre de la carpeta
+ * Ejemplo: /nombre_carpeta/
+ */
 define("RUTA", "/");
 
+/// Si no se pueden incluir los siguientes archivos se detiene la ejecución
 require_once('librerias/MysqlConexion.php');
 require_once('librerias/Controlador.php');
 require_once('librerias/ControlUrl.php');
@@ -9,5 +21,3 @@ require_once('librerias/Validar.php');
 require_once('librerias/Exception.php');
 require_once('librerias/PHPMailer.php');
 require_once('librerias/SMTP.php');
-
-?>
