58d376185ae8453ca3ef708f1fece10ffbcede1e
Zyz7 <51257260+Zyz7@users.noreply.github.com>
2021-03-04 19:50:45 -0600

    php

--- app/controladores/Login.php
+++ app/controladores/Login.php
@@ -50,6 +50,57 @@ class Login extends Controlador
       header("Location:".RUTA."login/registrate");
     }
   }
+  
+  /// \fn captcha Genera el captcha
+  function captcha()
+  {
+    session_start();
+  
+    $input = 'ABCDEFGHJKLMNPQRSTUVWXYZ';
+    $input_length = strlen($input);
+    $captcha_string = '';
+	  
+    for($i = 0; $i < 6; $i++) {
+        $random_character = $input[mt_rand(0, $input_length - 1)];
+        $captcha_string .= $random_character;
+    }
+ 
+    $_SESSION['captcha'] = $captcha_string;
+ 
+    $image = imagecreatetruecolor(200, 50);
+    imageantialias($image, true);
+    $colors = [];
+    $red = rand(125, 175);
+    $green = rand(125, 175);
+    $blue = rand(125, 175);
+	  
+    for($i = 0; $i < 5; $i++) {
+      $colors[] = imagecolorallocate($image, $red - 20*$i, $green - 20*$i, $blue - 20*$i);
+    }
+ 
+    imagefill($image, 0, 0, $colors[0]);
+ 
+    for($i = 0; $i < 10; $i++) {
+      imagesetthickness($image, rand(2, 10));
+      $line_color = $colors[rand(1, 4)];
+      imagerectangle($image, rand(-10, 190), rand(-10, 10), rand(-10, 190), rand(40, 60), $line_color);
+    }
+ 
+    $black = imagecolorallocate($image, 0, 0, 0);
+    $white = imagecolorallocate($image, 255, 255, 255);
+    $textcolors = [$black, $white];
+ 
+    for($i = 0; $i < 6; $i++) {
+      $letter_space = 170/6;
+      $initial = 15;
+      imagettftext($image, 24, rand(-15, 15), $initial + $i*$letter_space, rand(25, 45), 
+		   $textcolors[rand(0, 1)], $fonts[array_rand($fonts)], $captcha_string[$i]);
+    }
+ 
+    header('Content-type: image/png');
+    imagepng($image);
+    imagedestroy($image);  
+  }
 
   /// \fn registrate Guarda un nuevo usuario en la base de datos
   function registrate()
