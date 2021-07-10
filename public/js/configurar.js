5b299f9333a798e43da86edf85cdab29ac2c9172
zyz <zyz7@pm.me>
2021-07-10 11:58:58 -0500

    js

--- public/js/configurar.js
+++ public/js/configurar.js
@@ -1,4 +1,8 @@
 function tamañoFuente(tam) {
-  var tam=parseInt(tam);
-  document.body.style.fontSize=tam + "px";
+  var tam = parseInt(tam);
+  document.body.style.fontSize = tam + "px";
+}
+
+function colorFondo(color) {
+  document.body.style.backgroundColor = color;
 }
